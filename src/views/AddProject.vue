e1e80b2bc235295366d3b41230c0746ede4d866f
Sebastian <sebastian.chang23@gmail.com>
2021-02-15 18:47:26 -0600

    Created project planner

---
+++ src/views/AddProject.vue
@@ -0,0 +1,78 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <label>Title:</label>
+    <input v-model="title" type="text" required />
+    <label>Details:</label>
+    <textarea v-model="details" required></textarea>
+    <button>Add Project</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: '',
+      details: '',
+      uri: 'http://localhost:3000/projects/',
+    }
+  },
+  methods: {
+    handleSubmit () {
+      fetch(this.uri, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: this.title,
+          details: this.details,
+          complete: false
+        })
+      })
+        .then(() => this.$router.push('/'))
+        .catch(err => console.error(err))
+    }
+  }
+}
+</script>
+<style>
+form {
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+}
+label {
+  display: block;
+  color: #bbb;
+  text-transform: uppercase;
+  font-size: 14px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin: 20px 0 10px 0;
+}
+input {
+  padding: 10px;
+  border: 0;
+  border-bottom: 1px solid #ddd;
+  width: 100%;
+  box-sizing: border-box;
+}
+textarea {
+  border: 1px solid #ddd;
+  padding: 10px;
+  width: 100%;
+  box-sizing: border-box;
+  height: 100px;
+}
+form button {
+  display: block;
+  margin: 20px auto 0;
+  background: #00ce89;
+  color: white;
+  padding: 10px;
+  border: 0;
+  border-radius: 6px;
+  font-size: 16px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FilterNav
+      @filterChange="currentFilter = $event"
+      :currentFilter="currentFilter"
+    />
+    <div v-if="projects.length">
+      <div v-for="project in filteredProjects" :key="project.id">
+        <SingleProject
+          :project="project"
+          @delete="handleDelete"
+          @complete="handleComplete"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SingleProject from '../components/SingleProject'
+import FilterNav from '../components/FileNav'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: { SingleProject, FilterNav },
+  data () {
+    return {
+      projects: [],
+      currentFilter: 'all',
+    }
+  },
+  mounted () {
+    fetch('http://localhost:3000/projects')
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.error(err))
+  },
+  computed: {
+    filteredProjects () {
+      if (this.currentFilter === 'completed') {
+        return this.projects.filter(project => project.complete)
+      }
+      else if (this.currentFilter === 'onGoing') {
+        return this.projects.filter(project => !project.complete)
+      }
+      else {
+        return this.projects
+      }
+    }
+  },
+  methods: {
+    handleDelete (id) {
+      this.projects = this.projects.filter(project => project.id !== id)
+    },
+    handleComplete (id) {
+      const tempProject = this.projects.find(project => project.id === id)
+      tempProject.complete = !tempProject.complete
+    }
   }
 }
 </script>
